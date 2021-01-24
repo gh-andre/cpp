c3f915a528535f6cc25654c806420a202d76218d
Andre <gh-andre@users.noreply.github.com>
2021-01-24 09:42:17 -0500

    Added version and build number to the output

--- test.cpp
+++ test.cpp
@@ -1,7 +1,14 @@
 #include <cstdio>
 
+#define VERSION "1.1.0"
+
+#if !defined(BUILD_NUMBER)
+#error BUILD_NUMBER must be defined when compiling this source file
+#enfif    
+
 int main(void)
 {
-    printf("Hello, world\n");
+    printf("Hello, world!\n");
+    printf("\nv%s build %d\n", VERSION, BUILD_NUMBER);
     return 0;
 }
